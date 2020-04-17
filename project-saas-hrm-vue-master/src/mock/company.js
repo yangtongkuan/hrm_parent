b486b513856294334a89df05fcebe6d1b5ee51e9
yangtongkuan <yangtongkuan@qq.com>
2020-04-17 23:41:54 +0800

    管理后台企业列表模拟数据

--- project-saas-hrm-vue-master/src/mock/company.js
+++ project-saas-hrm-vue-master/src/mock/company.js
@@ -4,7 +4,7 @@ import { param2Obj } from '@/utils'
 const List = []
 const count = 100
 
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 10; i++) {
     let data = {
         id: '1' + i,
         name: '企业' + i,
@@ -12,19 +12,19 @@ for (let i = 0; i < 3; i++) {
         version: '试用版v1.0',
         renewalDate: '2018-01-01',
         expirationDate: '2019-01-01',
-        companyArea: 'string',
-        companyAddress: 'string',
+        companyArea: '山东济南历下区',
+        companyAddress: '力高国际',
         businessLicenseId: 'string',
-        legalRepresentative: 'string',
-        companyPhone: '13800138000',
-        mailbox: 'string',
-        companySize: 'string',
+        legalRepresentative: 'mr.yangtk',
+        companyPhone: '18854000080',
+        mailbox: 'yangtongkuan@qq.com',
+        companySize: '15人以下',
         industry: 'string',
         remarks: 'string',
-        auditState: 'string',
+        auditState: '1',
         state: '1',
-        balance: 'string',
-        createTime: 'string'
+        balance: '0.0',
+        createTime: '2020-12-12'
     }
   List.push(data)
 }
--- project-saas-hrm-vue-master/src/module-saas-clients/pages/index.vue
+++ project-saas-hrm-vue-master/src/module-saas-clients/pages/index.vue
@@ -2,7 +2,42 @@
   <div class="dashboard-container">
     <div class="app-container">
       <el-card shadow="never">
-        saas企业管理
+        <el-table :data="companyList" border stripe="true" style="width: 100%">
+          <el-table-column fixed type="index" label="序号" width="50"></el-table-column>
+          <el-table-column prop="name" label="企业名称" width="150"></el-table-column>
+          <el-table-column prop="version" label="当前版本" width="100"></el-table-column>
+          <el-table-column prop="expirationDate" label="到期时间" width="100"></el-table-column>
+          <el-table-column prop="companyArea" label="公司地区" width="150"></el-table-column>
+          <el-table-column prop="companyAddress" label="公司地址" width="150"></el-table-column>
+          <el-table-column prop="legalRepresentative" label="法人代表" width="100"></el-table-column>
+          <el-table-column prop="companyPhone" label="公司电话" width="150"></el-table-column>
+          <el-table-column prop="mailbox" label="邮箱" width="150"></el-table-column>
+          <el-table-column prop="auditState" label="审核状态" width="150"></el-table-column>
+          <el-table-column prop="state" label="状态" width="150">
+            <!-- 开关组件
+              active-value : 激活时数据值
+              inactive-value : 未激活时数据值
+              active-color： 激活时颜色
+              inactive-color： 未激活时颜色
+            -->
+            <template slot-scope="scope">
+              <el-switch
+                v-model="scope.row.state"
+                active-color="#13ce66"
+                inactive-color="#ff4949"
+                active-value="1"
+                inactive-value="0" disabled>
+              </el-switch>
+            </template>
+          </el-table-column>
+          <el-table-column prop="balance" label="余额" width="150"></el-table-column>
+          <el-table-column fixed="right" label="操作" width="100">
+            <template slot-scope="scope">
+              <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+              <!-- <el-button type="text" size="small">编辑</el-button> -->
+            </template>
+          </el-table-column>
+        </el-table>
       </el-card>
      </div>
   </div>
@@ -13,15 +48,16 @@ import {list} from '@/api/base/saasclients'
 export default {
   data() {
     return {
-      getCompanyList: function() {
-        list().then(res => {
-          console.log(res.data.data)
-        })
-      }
-     
+     companyList: []
     }
   },
   methods: {
+    getCompanyList: function() {
+        list().then(res => {
+          console.log(res.data.data)
+          this.companyList = res.data.data
+        })
+      }
   },
   // 创建完毕状态
   created() {
@@ -31,6 +67,13 @@ export default {
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
+// body .el-table th.gutter{
+//     display: table-cell!important;
+// }
+.el-table--border th.gutter:last-of-type {
+    display: block !important;
+    width: 17px !important;
+}
 .alert {
   margin: 10px 0px 0px 0px;
 }
